08201141dea7810e9f007fdf897d9f026c4b4df4
RebbelPigman <rebbel.pigman@gmail.com>
2026-09-11 10:28:13 +0200

    add proton GE to steam

--- modules/features/system/game.nix
+++ modules/features/system/game.nix
@@ -17,6 +17,9 @@
         remotePlay.openFirewall = true;
         dedicatedServer.openFirewall = true;
         localNetworkGameTransfers.openFirewall = true;
+        extraCompatPackages = with pkgs; [
+          proton-ge-bin
+        ];
       };
     };
   };
